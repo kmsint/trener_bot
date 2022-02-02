742c78ca51ca9212b2bf680dc354b7f8b9580df0
Mikhail Kryzhanovskiy <kmsrus@gmail.com>
2022-02-02 06:22:11 +0300

    fixed function in services.py

--- trener_bot/services.py
+++ trener_bot/services.py
@@ -10,7 +10,7 @@ def connect_to_gs(gs_document):
 
 
 def get_current_date():
-    return datetime.now().strftime('%d-%m-%Y')
+    return datetime.now().strftime('%-d-%m-%Y')
 
 
 def get_training_by_date(date):
@@ -24,24 +24,25 @@ def get_training_by_date(date):
 
 def get_next_training():
     wks = connect_to_gs(GS_DOCUMENT)
-    current_date = datetime.now()
+    next_date = datetime.now()
     while True:
-        next_date = (current_date + timedelta(days=1)).strftime('%d-%m-%Y')
-        current_date_cell = wks.find(next_date)
+        next_date += timedelta(days=1)
+        next_date_format = next_date.strftime('%-d-%m-%Y')
+        current_date_cell = wks.find(next_date_format)
         if current_date_cell:
             next_training = wks.get_row(current_date_cell[0].row, include_tailing_empty=False)
             return next_training[0], next_training[1:]
     
 
 def get_workouts_left():
-    
+
     wks = connect_to_gs(GS_DOCUMENT)
     next_training_date = get_next_training()[0]
     training_dict = {}
     all_dates = wks.get_col(1, include_tailing_empty=False)[1:]
     all_dates_left = all_dates[all_dates.index(next_training_date):]
     training_table = wks.get_all_values(returnas='matrix', majdim='ROWS', include_tailing_empty=False, include_tailing_empty_rows=False)[1:]
-    
+
     for row in training_table:
         if row[0] in all_dates_left:
             for elem in row[1:]:
@@ -90,3 +91,5 @@ def format_output(training_dict):
 def format_training_output(training_tuple):
     training = '\n'.join(training_tuple[1])
     return f'Тренировка {training_tuple[0]}:\n{training}'
+
+print(format_output(get_workouts_left()))
